9b165ceac5ae6d8279057603636e7d8cabdc92d3
Ryan <Ryan@10.196.51.24>
2018-04-24 17:48:45 +1200

    Game env change only, Game now setup

--- Game_Environment.java
+++ Game_Environment.java
@@ -2,20 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game_Environment {
-	private String teamName;
-	private int numberHeroes;
+	private static String teamName;
+	private static int numberHeroes;
 	private static int numberCities;	
-	
 	private ArrayList<Villain> list_villains = new ArrayList<Villain>();
-
 	
 	private static void getTeamName() {
 		Boolean incorrectInput = true;
-		
 		while(incorrectInput) {
 			System.out.println("Enter your team name: ");
 			Scanner scanner = new Scanner(System.in);
-			String teamName = scanner.nextLine();
+			teamName = scanner.nextLine();
 			
 			if (teamName.length() >= 2 && teamName.length() <= 10) {
 				incorrectInput = false;
@@ -24,11 +21,8 @@ public class Game_Environment {
 			else {
 				System.out.println("Name must be between 2 and 10 characters long.");
 			}
-		}
-		
-	}
-	
-	
+		}	
+	}	
 	private static void getNumberCities() {
 		Boolean incorrectInput = true;
 		
@@ -36,19 +30,30 @@ public class Game_Environment {
 			System.out.println("Enter the number of cities your heroes need to explore: ");
 			Scanner scanner = new Scanner(System.in);
 			String input = scanner.nextLine();
-			int numberCities = (Integer.parseInt(input));
+			numberCities = (Integer.parseInt(input));
 			if (numberCities <= 6 && numberCities >= 3) {
 				incorrectInput = false;
 				System.out.println("Num Cities to explore " + numberCities);
-			}
-			else {
+			} else {
 				System.out.println("Choose between 3 and 6 cities.");
 			}
 		}
 	}
 	
-	private void getNumberHeroes() {
-		
+	private static void getNumberHeroes() {
+		Boolean incorrectInput = true;
+		while(incorrectInput) {
+			System.out.println("Enter however many Heroes you want on your team: ");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
+			numberHeroes = (Integer.parseInt(input));
+			if (numberHeroes <= 3 && numberHeroes >= 1) {
+				incorrectInput = false;
+				System.out.println("You have chosen to take " + numberHeroes + " Heroes on this adventure with you");
+			} else {
+				System.out.println("You can only have between 1 and 3 Heroes");
+			}
+		}
 	}
 	
 	private void initTeam(String teamName, int numberHeroes) {
@@ -59,7 +64,9 @@ public class Game_Environment {
 	
 	
 	public static void main(String[] args) {
+		getTeamName();
 		getNumberCities();
+		getNumberHeroes();
 		
 	}
 	
