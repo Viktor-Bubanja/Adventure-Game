da5dee8123afe79eb5fc7b79ce74544b43d316ec
RyanSheridan11 <38237869+RyanSheridan11@users.noreply.github.com>
2018-04-24 16:05:33 +1200

    Update Game_Environment.java

--- Game_Environment.java
+++ Game_Environment.java
@@ -4,20 +4,47 @@ import java.util.Scanner;
 public class Game_Environment {
 	private String teamName;
 	private int numberHeroes;
-	private int numberCities;	
+	private static int numberCities;	
+	
 	private ArrayList<Villain> list_villains = new ArrayList<Villain>();
 
 	
-	private void getTeamName() {
-		System.out.println("Enter your team name: ");
-		Scanner scanner = new Scanner(System.in);
-		String teamName = scanner.nextLine();
-		System.out.println("Your team is called " + teamName);
+	private static void getTeamName() {
+		Boolean incorrectInput = true;
+		
+		while(incorrectInput) {
+			System.out.println("Enter your team name: ");
+			Scanner scanner = new Scanner(System.in);
+			String teamName = scanner.nextLine();
+			
+			if (teamName.length() >= 2 && teamName.length() <= 10) {
+				incorrectInput = false;
+				System.out.println("Your team is called " + teamName);
+			}
+			else {
+				System.out.println("Name must be between 2 and 10 characters long.");
+			}
+		}
+		
 	}
 	
 	
-	private void getNumberCities() {
+	private static void getNumberCities() {
+		Boolean incorrectInput = true;
 		
+		while(incorrectInput) {
+			System.out.println("Enter the number of cities your heroes need to explore: ");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
+			int numberCities = (Integer.parseInt(input));
+			if (numberCities <= 6 && numberCities >= 3) {
+				incorrectInput = false;
+				System.out.println("Num Cities to explore " + numberCities);
+			}
+			else {
+				System.out.println("Choose between 3 and 6 cities.");
+			}
+		}
 	}
 	
 	private void getNumberHeroes() {
@@ -31,7 +58,10 @@ public class Game_Environment {
 	}
 	
 	
-	
+	public static void main(String[] args) {
+		getNumberCities();
+		
+	}
 	
 	
 	
