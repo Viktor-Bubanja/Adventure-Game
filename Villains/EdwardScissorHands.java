5364facc56367183cb48007f83a47489232fa52d
RyanSheridan11 <38237869+RyanSheridan11@users.noreply.github.com>
2018-05-24 19:48:12 +1200

    Delete EdwardScissorHands.java

--- Villains/EdwardScissorHands.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class EdwardScissorHands extends Villain {
-	public EdwardScissorHands(ArrayList<String> games) {
-		super("I'm gonna cut you up", games, "Edward Scissor Hands", 70);
-	}
-}
