912b140490e2f0b16430aa46e582851dce9dae69
RyanSheridan11 <38237869+RyanSheridan11@users.noreply.github.com>
2018-05-24 19:48:45 +1200

    Delete TheMasterMind.java

--- Villains/TheMasterMind.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class TheMasterMind extends Villain {
-	public TheMasterMind(ArrayList<String> games) {
-		super("You can try...", games, "The Master Mind", 70);
-	}
-}
