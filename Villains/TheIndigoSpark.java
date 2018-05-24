ea2c4bdd65b4eb006237663916ede3c2385bf9f9
RyanSheridan11 <38237869+RyanSheridan11@users.noreply.github.com>
2018-05-24 19:48:39 +1200

    Delete TheIndigoSpark.java

--- Villains/TheIndigoSpark.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class TheIndigoSpark extends Villain{
-	public TheIndigoSpark(ArrayList<String> games) {
-		super("Go home and be a family man!", games, "The Indigo Spark", 100);
-	}
-}
