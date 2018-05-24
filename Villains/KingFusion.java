8b0f8c41b632869ac3d355314e2b39c0b0952842
RyanSheridan11 <38237869+RyanSheridan11@users.noreply.github.com>
2018-05-24 19:48:21 +1200

    Delete KingFusion.java

--- Villains/KingFusion.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class KingFusion extends Villain {
-	public KingFusion(ArrayList<String> games) {
-		super("You spoony bard!", games, "King Fusion", 50);
-	}
-}
